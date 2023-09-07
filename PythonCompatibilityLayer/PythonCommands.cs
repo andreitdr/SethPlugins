125fdf8262f988addf89b060be1c099d48e1a070
Tudor Andrei <tudor.andrei.707@gmail.com>
2023-09-07 13:54:27 +0300

    Initial commit2

--- PythonCompatibilityLayer/PythonCommands.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PythonCompatibilityLayer;
-
-public class PythonCommands : PluginManager.Others.SettingsDictionary<string, string>
-{
-    public PythonCommands(string? file) : base(file)
-    {
-    }
-    
-}
--- PythonCompatibilityLayer/Variables.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PythonCompatibilityLayer;
-
-public class Variables
-{
-    public static PythonCommands Commands = new PythonCommands("./Data/Resources/pythonCommands.json");
-}
