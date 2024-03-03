78f17db79e5a57361653240ab09711366843b166
Andrei Tudor <tudor.andrei.707@gmail.com>
2024-03-03 18:00:44 +0200

    Added Basic UI Functionality

--- MusicPlayer/Events/OnLoad.cs
+++ MusicPlayer/Events/OnLoad.cs
@@ -8,11 +8,12 @@ public class OnLoad: DBEvent
 {
     public string Name => "Music Commands";
     public string Description => "The default music commands event loader";
-    public void Start(DiscordSocketClient client)
+    public async void Start(DiscordSocketClient client)
     {
         var path1    = Functions.dataFolder + "Music/";
         var fileName = path1 + "music_db.json";
         Directory.CreateDirectory(path1);
         Variables._MusicDatabase = new MusicDatabase(fileName);
+        await Variables._MusicDatabase.LoadFromFile();
     }
 }
